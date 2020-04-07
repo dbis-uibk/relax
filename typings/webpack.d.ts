e9133a89460ae9339d80a6f6f70f940d8d31ba49
Michael Rampl <info@michael-rampl.com>
2020-04-07 09:12:47 +0200

    second test release

--- typings/webpack.d.ts
+++
@@ -1,7 +0,0 @@
-// src: https://github.com/TypeStrong/ts-loader#code-splitting-and-loading-other-resources
-
-declare var require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};

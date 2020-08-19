13d84526a3cfa85b2d2af8372dcc2f43e8304d90
Michael Rampl <michaelrampl@sadion.net>
2020-08-19 16:57:34 +0200

    Modified Help, added license comment block to 2 source files

--- src/calc2/components/navigation-mobile.tsx
+++ src/calc2/components/navigation-mobile.tsx
@@ -1,3 +1,9 @@
+/*** Copyright 2018 Johannes Kessler
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
--- src/calc2/views/help.tsx
+++ src/calc2/views/help.tsx
@@ -2628,17 +2628,7 @@ export class Help extends React.Component<Props> {
 										src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" 
 									/>
 								</a><br />
-								This <a 
-									// xmlns:dct="http://purl.org/dc/terms/" 
-									href="http://purl.org/dc/dcmitype/Text" 
-									rel="dct:type"
-								>work</a> by
-								<a 
-									// xmlns:cc="http://creativecommons.org/ns#" 
-									href="http://dbis-uibk.github.io/relax/help.htm" 
-									property="cc:attributionName" 
-									rel="cc:attributionURL">Johannes Kessler</a>
-								is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+								This document by Johannes Kessler is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 							</p>
 						</div>
 					</div>

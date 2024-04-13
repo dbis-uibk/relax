16e77094315947352721ec4c5714ac7606dec61e
Rodrigo Laiola Guimarães <rlaiola@gmail.com>
2024-04-13 13:27:37 -0300

    Update Selection.ts

--- src/db/exec/Selection.ts
+++ src/db/exec/Selection.ts
@@ -9,7 +9,6 @@ import { RANode, RANodeUnary, Session } from './RANode';
 import { Schema } from './Schema';
 import { Table } from './Table';
 import * as ValueExpr from './ValueExpr';
-import { RenameRelation } from './RenameRelation';
 
 
 export class Selection extends RANodeUnary {
@@ -62,16 +61,90 @@ export class Selection extends RANodeUnary {
 		try {
 			this._condition.check(this._schema);
 		} catch (e) {
+			// Second try: check wether condition uses a relation alias
 			const relAlias = this._child.getMetaData('fromVariable');
 			if (relAlias) {
-				const altChild = 
-					new RenameRelation(this._child, relAlias + "");
-				altChild.check();
-				this._schema = altChild.getSchema();
+				// All columns names
+				let allCols: (string | number)[] = [];
+				// Ambiguous columns names
+				let blacklist: (string | number)[] = [];
+				const numCols = this._schema.getSize();
+				for (let i = 0; i < numCols; i++) {
+					allCols.push(this._schema.getColumn(i).getName());
+
+					// If column already in blacklist, skip it
+					// Cannot set relation alias for this column
+					if (blacklist.includes(this._schema.getColumn(i).getName())) {
+						continue;
+					}
+
+					for (let j = i+1; j < numCols; j++) {
+						// If found a sibling column, cannot set relation alias
+						if (this._schema.getColumn(i).getName() === this._schema.getColumn(j).getName()) {
+							blacklist.push(this._schema.getColumn(i).getName());
+							break;
+						}
+					}
+				}
+
+				// Unique columns names
+				let whitelist = allCols.filter(x => !blacklist.includes(x));
+				
+				// Generate all column combinations
+				// https://stackoverflow.com/questions/43241174/javascript-generating-all-combinations-of-elements-in-a-single-array-in-pairs
+				let combCols = [];
+				let temp = [];
+				let slent = Math.pow(2, whitelist.length);
+
+				for (let i = 0; i < slent; i++) {
+					temp = [];
+					for (var j = 0; j < whitelist.length; j++) {
+						if ((i & Math.pow(2, j))) {
+							temp.push(whitelist[j]);
+						}
+					}
+					if (temp.length > 0) {
+						combCols.push(temp);
+					}
+				}
+
+				// Apply and test if relation alias works for any combination of
+				// unique columns
+				for (let i = 0; i < combCols.length; i++) {
+					let newSchema = this._schema.copy();
+
+					for (let j = 0; j < combCols[i].length; j++) {
+
+						for (let k = 0; k < numCols; k++) {
+							if (combCols[i][j] === newSchema.getColumn(k).getName()) {
+								newSchema.setRelAlias(relAlias, k);
+								break;
+							}
+						}
+
+					}
+
+					// Check if combination of relation alias works
+					try {
+						this._condition.check(newSchema);
+					}
+					catch (e) {
+						// Test failed, try next combination
+						continue;
+					}
+
+					if (this._condition.getDataType() === 'boolean') {
+						this._schema = newSchema;
+						return;
+					}
+
+				}
+
+				// If no combination of relation alias works
+				this.throwExecutionError(e.message);
 			}
-			this._condition.check(this._schema);
 		}
-		
+
 		if (this._condition.getDataType() !== 'boolean') {
 			this.throwExecutionError(i18n.t('db.messages.exec.error-condition-must-be-boolean'));
 		}

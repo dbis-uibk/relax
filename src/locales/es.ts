96527e4e9bbc75fb6af07097eb03f5b7538effa6
Rodrigo Laiola Guimarães <rlaiola@users.noreply.github.com>
2026-05-22 17:31:21 -0300

    Update toolbar documentation

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -227,9 +227,15 @@ export const langES: Partial<typeof langEN> = {
     '<div><b>(</b> A <b class="math">) ⟗ A.a != B.a (</b> B <b>)</b></div>',
   ].join('\n'),
   'calc.editors.ra.toolbar.left-semi-join': 'left semi join',
-  'calc.editors.ra.toolbar.left-semi-join-content': '<div><b>(</b> A <b class="math">) ⋉ (</b> B <b>)</b></div>',
+  'calc.editors.ra.toolbar.left-semi-join-content': `
+    <div><b>(</b> A <b class="math">) ⋉ (</b> B <b>)</b></div>
+    <div><b>(</b> A <b class="math">) ⋉ A.a ≤ B.a (</b> B <b>)</b></div>
+  `,
   'calc.editors.ra.toolbar.right-semi-join': 'right semi join',
-  'calc.editors.ra.toolbar.right-semi-join-content': '<div><b>(</b> A <b class="math">) ⋊ (</b> B <b>)</b></div>',
+  'calc.editors.ra.toolbar.right-semi-join-content':  `
+    '<div><b>(</b> A <b class="math">) ⋊ (</b> B <b>)</b></div>
+    <div><b>(</b> A <b class="math">) ⋊ A.a = B.a (</b> B <b>)</b></div>
+  `,
   'calc.editors.ra.toolbar.anti-join': 'anti join',
   'calc.editors.ra.toolbar.anti-join-content': '<div><b>(</b> A <b class="math">) ▷ (</b> B <b>)</b></div>',
   'calc.editors.ra.toolbar.assignment': 'asignación',
